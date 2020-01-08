a707ab0ea54fd28d1cfbe32b5ac71050938afb01
ljurak <lucas.jurak@gmail.com>
2020-01-08 21:59:23 +0100

    Add controller for user queries

---
+++ src/main/java/com/expense/app/user/controller/UserQueryController.java
@@ -0,0 +1,17 @@
+package com.expense.app.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.expense.app.user.dto.command.UserRegisterCommand;
+
+@Controller
+public class UserQueryController {
+	
+	@GetMapping("/register-user")
+	public String showRegisterForm(Model model) {
+		model.addAttribute("userCommand", new UserRegisterCommand());
+		return "register-user";
+	}
+}
