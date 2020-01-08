f97290ef6e380658c7deac5ceb7cada072724a7c
ljurak <lucas.jurak@gmail.com>
2020-01-08 21:59:07 +0100

    Add controller for user commands

---
+++ src/main/java/com/expense/app/user/controller/UserCommandController.java
@@ -0,0 +1,30 @@
+package com.expense.app.user.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.expense.app.user.dto.command.UserRegisterCommand;
+import com.expense.app.user.service.UserCommandService;
+
+@Controller
+public class UserCommandController {
+	
+	private UserCommandService userService;
+	
+	public UserCommandController(UserCommandService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping("/register-user")
+	public String registerUser(@ModelAttribute("userCommand") @Valid UserRegisterCommand command, BindingResult result) {
+		if (result.hasErrors()) {
+			return "register-user";
+		}
+		userService.registerUser(command);
+		return "redirect:/login";
+	}
+}
