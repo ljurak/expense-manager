37fe072c01a1aa00cfe3b7dd5f579815dbe76dc8
ljurak <lucas.jurak@gmail.com>
2020-01-08 21:51:31 +0100

    Initial commit

---
+++ src/test/java/com/expense/app/ExpenseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expense.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
