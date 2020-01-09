17804276ab718cfc8c15c42696547eb14a2f7697
ljurak <lucas.jurak@gmail.com>
2020-01-09 15:27:41 +0100

    Add test user data

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,8 @@
 INSERT INTO roles (name) VALUES ('ROLE_USER');
 
 INSERT INTO categories (name) VALUES ('food'), ('drink'), ('sport'), ('fun');
+
+INSERT INTO users (username, password, firstname, lastname, email, enabled) VALUES
+	('ananas', '{noop}ananas', 'Anatol', 'Mongoł', 'among@mail.com', 1);
+	
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
