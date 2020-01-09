87634ab7a541cdd8b8fe1a7549e0703fc7b0b2e5
ljurak <lucas.jurak@gmail.com>
2020-01-09 15:31:34 +0100

    Add list of expenses to model

--- src/main/java/com/expense/app/expense/controller/ExpenseQueryController.java
+++ src/main/java/com/expense/app/expense/controller/ExpenseQueryController.java
@@ -7,14 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.expense.app.expense.dto.command.ExpenseCreateCommand;
 import com.expense.app.expense.repo.CategoryRepo;
+import com.expense.app.expense.repo.ExpenseRepo;
 
 @Controller
 @RequestMapping("/expenses")
 public class ExpenseQueryController {
 	
+	private ExpenseRepo expenseRepo;
+	
 	private CategoryRepo categoryRepo;
 	
-	public ExpenseQueryController(CategoryRepo categoryRepo) {
+	public ExpenseQueryController(ExpenseRepo expenseRepo, CategoryRepo categoryRepo) {
+		this.expenseRepo = expenseRepo;
 		this.categoryRepo = categoryRepo;
 	}
 	
@@ -22,6 +26,7 @@ public class ExpenseQueryController {
 	public String showExpensesPage(Model model) {
 		model.addAttribute("expenseCommand", new ExpenseCreateCommand());
 		model.addAttribute("expenseCategories", categoryRepo.findAll());
+		model.addAttribute("expensesList", expenseRepo.findAll());
 		return "home";
 	}
 }
