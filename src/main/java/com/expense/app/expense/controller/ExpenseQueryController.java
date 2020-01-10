a6324a2fa592443c093b364c5fd5fedacf0037b4
ljurak <lucas.jurak@gmail.com>
2020-01-10 23:18:33 +0100

    Add paging to expenses list

--- src/main/java/com/expense/app/expense/controller/ExpenseQueryController.java
+++ src/main/java/com/expense/app/expense/controller/ExpenseQueryController.java
@@ -1,13 +1,18 @@
 package com.expense.app.expense.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.expense.app.expense.dto.command.ExpenseCreateCommand;
+import com.expense.app.expense.entity.ExpenseEntity;
 import com.expense.app.expense.repo.CategoryRepo;
 import com.expense.app.expense.repo.ExpenseRepo;
 
@@ -25,12 +30,22 @@ public class ExpenseQueryController {
 	}
 	
 	@GetMapping
-	public String showExpensesPage(Authentication authentication, Model model) {
+	public String showExpensesPage(
+			@RequestParam(required = false, defaultValue = "0") int page, 
+			@RequestParam(required = false, defaultValue = "15") int size, 
+			Authentication authentication, 
+			Model model) {
+		
 		String username = ((UserDetails) authentication.getPrincipal()).getUsername();
+		
+		Page<ExpenseEntity> expensesPage = expenseRepo.findByUsername(
+				username, PageRequest.of(page, size, Sort.by("date").descending()));
 		
 		model.addAttribute("expenseCommand", new ExpenseCreateCommand());
 		model.addAttribute("expenseCategories", categoryRepo.findAll());
-		model.addAttribute("expensesList", expenseRepo.findByUsername(username));
+		model.addAttribute("expensesList", expensesPage.getContent());
+		model.addAttribute("pageCount", expensesPage.getTotalPages());
+		model.addAttribute("currentPage", expensesPage.getNumber());
 		return "home";
 	}
 }
