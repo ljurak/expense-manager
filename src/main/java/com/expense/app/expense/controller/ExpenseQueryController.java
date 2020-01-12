422427fc23a7f0d92906e61e7bffb84bcae2a361
ljurak <lucas.jurak@gmail.com>
2020-01-12 20:33:47 +0100

    Add method for filtering expenses

--- src/main/java/com/expense/app/expense/controller/ExpenseQueryController.java
+++ src/main/java/com/expense/app/expense/controller/ExpenseQueryController.java
@@ -1,5 +1,7 @@
 package com.expense.app.expense.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -7,17 +9,20 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.expense.app.expense.dto.command.ExpenseCreateCommand;
+import com.expense.app.expense.dto.query.ExpenseFilterQuery;
+import com.expense.app.expense.entity.CategoryEntity;
 import com.expense.app.expense.entity.ExpenseEntity;
 import com.expense.app.expense.repo.CategoryRepo;
 import com.expense.app.expense.repo.ExpenseRepo;
+import com.expense.app.expense.repo.PageWrapper;
 
 @Controller
-@RequestMapping("/expenses")
 public class ExpenseQueryController {
 	
 	private ExpenseRepo expenseRepo;
@@ -29,7 +34,12 @@ public class ExpenseQueryController {
 		this.categoryRepo = categoryRepo;
 	}
 	
-	@GetMapping
+	@ModelAttribute("expenseCategories")
+	public Iterable<CategoryEntity> populateCategories() {
+		return categoryRepo.findAll();
+	}
+	
+	@GetMapping("/expenses")
 	public String showExpensesPage(
 			@RequestParam(required = false, defaultValue = "0") int page, 
 			@RequestParam(required = false, defaultValue = "15") int size, 
@@ -41,11 +51,37 @@ public class ExpenseQueryController {
 		Page<ExpenseEntity> expensesPage = expenseRepo.findByUsername(
 				username, PageRequest.of(page, size, Sort.by("date").descending()));
 		
-		model.addAttribute("expenseCommand", new ExpenseCreateCommand());
-		model.addAttribute("expenseCategories", categoryRepo.findAll());
+		model.addAttribute("expenseCreateCommand", new ExpenseCreateCommand());
+		model.addAttribute("expenseFilterQuery", new ExpenseFilterQuery());
 		model.addAttribute("expensesList", expensesPage.getContent());
 		model.addAttribute("pageCount", expensesPage.getTotalPages());
 		model.addAttribute("currentPage", expensesPage.getNumber());
 		return "home";
+	}
+	
+	@GetMapping("/expenses/show")
+	public String showFilteredExpensesPage(
+			@ModelAttribute("expenseFilterQuery") @Valid ExpenseFilterQuery query, 
+			BindingResult result, 
+			@RequestParam(required = false, defaultValue = "0") int page, 
+			Authentication authentication, 
+			Model model) {
+		
+		model.addAttribute("expenseCreateCommand", new ExpenseCreateCommand());
+		
+		if (result.hasErrors()) {
+			return "home";
+		}
+		
+		String username = ((UserDetails) authentication.getPrincipal()).getUsername();
+		query.setUsername(username);
+		
+		PageWrapper<ExpenseEntity> pageWrapper = expenseRepo.findAllByFilter(query, PageRequest.of(page, 15, Sort.by("date").descending()));
+		
+		model.addAttribute("expensesList", pageWrapper.getContent());
+		model.addAttribute("pageCount", pageWrapper.getTotalPages());
+		model.addAttribute("currentPage", pageWrapper.getCurrentPage());
+		
+		return "expenseFilter";
 	}
 }
