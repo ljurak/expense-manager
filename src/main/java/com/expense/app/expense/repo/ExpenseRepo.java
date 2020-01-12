210e829bf5d2d92e0779f9d768000dbfa1cc2f38
ljurak <lucas.jurak@gmail.com>
2020-01-13 00:07:30 +0100

    Remove unused interface

--- src/main/java/com/expense/app/expense/repo/ExpenseRepo.java
+++ src/main/java/com/expense/app/expense/repo/ExpenseRepo.java
@@ -2,6 +2,7 @@ package com.expense.app.expense.repo;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import com.expense.app.expense.entity.ExpenseEntity;
 
 @Repository
-public interface ExpenseRepo extends CrudRepository<ExpenseEntity, Long>, CustomizedExpenseRepo {
+public interface ExpenseRepo extends CrudRepository<ExpenseEntity, Long>, JpaSpecificationExecutor<ExpenseEntity> {
 	
 	@Query("select e from ExpenseEntity e where e.user.username = :username")
 	Page<ExpenseEntity> findByUsername(@Param("username") String username, Pageable pageable);
