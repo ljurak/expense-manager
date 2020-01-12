b712fb4781ee3ad47e4d0f2cee479b4a93d55bdc
ljurak <lucas.jurak@gmail.com>
2020-01-12 20:47:33 +0100

    Bugfix for query

--- src/main/java/com/expense/app/user/repo/UserRepo.java
+++ src/main/java/com/expense/app/user/repo/UserRepo.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.expense.app.user.entity.UserEntity;
@@ -11,8 +12,8 @@ import com.expense.app.user.entity.UserEntity;
 @Repository
 public interface UserRepo extends CrudRepository<UserEntity, Long> {
 	
-	@Query("select u from UserEntity u join fetch u.roles")
-	Optional<UserEntity> findByUsername(String username);
+	@Query("select u from UserEntity u join fetch u.roles where u.username = :username")
+	Optional<UserEntity> findByUsername(@Param("username") String username);
 	
 	boolean existsByUsername(String username);
 }
