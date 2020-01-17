df8dc381a05e20ebc0bcd05f3e434d70f87ccb87
ljurak <lucas.jurak@gmail.com>
2020-01-17 23:50:05 +0100

    Add dto for report results

---
+++ src/main/java/com/expense/app/expense/dto/ExpenseReportDto.java
@@ -0,0 +1,81 @@
+package com.expense.app.expense.dto;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.expense.app.expense.entity.CategoryEntity;
+
+public class ExpenseReportDto {
+	
+	private BigDecimal minExpense;
+	
+	private BigDecimal maxExpense;
+	
+	private BigDecimal avgExpense;
+	
+	private BigDecimal sumExpense;
+	
+	private Map<CategoryEntity, BigDecimal> expenseByCategory = new HashMap<>();
+
+	public BigDecimal getMinExpense() {
+		return minExpense;
+	}
+
+	public BigDecimal getMaxExpense() {
+		return maxExpense;
+	}
+
+	public BigDecimal getAvgExpense() {
+		return avgExpense;
+	}
+
+	public BigDecimal getSumExpense() {
+		return sumExpense;
+	}
+
+	public Map<CategoryEntity, BigDecimal> getExpenseByCategory() {
+		return expenseByCategory;
+	}
+	
+	public static Builder builder() {
+		return new Builder();
+	}
+	
+	public static final class Builder {
+		
+		private ExpenseReportDto report = new ExpenseReportDto();
+		
+		private Builder() {
+		}
+		
+		public Builder minExpense(BigDecimal minExpense) {
+			report.minExpense = minExpense;
+			return this;
+		}
+		
+		public Builder maxExpense(BigDecimal maxExpense) {
+			report.maxExpense = maxExpense;
+			return this;
+		}
+		
+		public Builder avgExpense(BigDecimal avgExpense) {
+			report.avgExpense = avgExpense;
+			return this;
+		}
+		
+		public Builder sumExpense(BigDecimal sumExpense) {
+			report.sumExpense = sumExpense;
+			return this;
+		}
+		
+		public Builder expenseByCategory(Map<CategoryEntity, BigDecimal> expenseByCategory) {
+			report.expenseByCategory = expenseByCategory;
+			return this;
+		}
+		
+		public ExpenseReportDto build() {
+			return report;
+		}
+	}
+ }
