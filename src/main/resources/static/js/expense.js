9b7b430c57f8a165d1789bccd15061846616a9bf
ljurak <lucas.jurak@gmail.com>
2020-01-27 23:50:44 +0100

    Add modal window to confirm expense removal

---
+++ src/main/resources/static/js/expense.js
@@ -0,0 +1,47 @@
+function openModal(e) {
+	e.preventDefault();
+	const modal = document.querySelector('.modal');
+	
+	const modalContent = document.createElement('div');
+	modalContent.classList.add('modal-cnt');
+	
+	const confirmationText = document.createElement('p');
+	confirmationText.textContent = 'Are you sure you want to remove this item?';
+	
+	const removeButton = document.createElement('a');
+	removeButton.classList.add('remove-btn');
+	removeButton.textContent = 'Remove';
+	removeButton.href = e.target.href;
+	
+	const closeButton = document.createElement('button');
+	closeButton.type = 'button';
+	closeButton.textContent = 'X';
+	closeButton.classList.add('close-btn');
+	closeButton.addEventListener('click', closeModal);
+	
+	modalContent.appendChild(confirmationText);
+	modalContent.appendChild(removeButton);
+	modalContent.appendChild(closeButton);
+	modal.appendChild(modalContent);
+	
+	modal.classList.add('open');
+}
+
+function closeModal() {
+	const modal = document.querySelector('.modal');
+	const modalContent = modal.querySelector('.modal-cnt');
+	
+	if (modalContent !== null) {
+		modal.classList.remove('open');
+		
+		modalContent.remove();
+		modalContent = null;
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	const removeButtons = document.querySelectorAll('.expenses-list .remove-btn');
+	for (const btn of removeButtons) {
+		btn.addEventListener('click', openModal);
+	}
+});
