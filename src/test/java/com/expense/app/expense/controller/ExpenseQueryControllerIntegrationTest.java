3a8d01f0635a087c555b54f155f037f64eea45d1
ljurak <lucas.jurak@gmail.com>
2020-01-31 21:46:57 +0100

    Add tests for generating pdf report

--- src/test/java/com/expense/app/expense/controller/ExpenseQueryControllerIntegrationTest.java
+++ src/test/java/com/expense/app/expense/controller/ExpenseQueryControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -75,5 +76,48 @@ public class ExpenseQueryControllerIntegrationTest {
 		.andExpect(status().isOk())
 		.andExpect(view().name("reportPage"))
 		.andExpect(model().attributeExists("report"));
+	}
+	
+	@Test
+	@WithMockUser("admin")
+	public void shouldFailValidationWhenGeneratingReportWithInvalidParameters() throws Exception {
+		// when
+		mockMvc.perform(get("/expenses/report")
+				.param("reportStartDate", "18-12-2019")
+				.param("reportEndDate", "15-10-2019"))
+		
+		// then
+		.andExpect(status().isOk())
+		.andExpect(view().name("reportPage"))
+		.andExpect(model().attributeExists("expenseFilterQuery"))
+		.andExpect(model().attributeExists("expenseCreateCommand"))
+		.andExpect(model().attributeExists("expenseReportQuery"));
+	}
+	
+	@Test
+	@WithMockUser("admin")
+	public void shouldGeneratePdfReport() throws Exception {
+		// when
+		mockMvc.perform(get("/expenses/report/print")
+				.param("reportStartDate", "2019-06-18")
+				.param("reportEndDate", "2019-12-31"))
+		
+		// then
+		.andExpect(status().isOk())
+		.andExpect(header().string("Content-Type", "application/pdf"))
+		.andExpect(header().exists("Content-Disposition"))
+		.andExpect(content().contentType(MediaType.APPLICATION_PDF));
+	}
+	
+	@Test
+	@WithMockUser("admin")
+	public void shouldNotGeneratePdfReportWhenInvalidParameters() throws Exception {
+		// when
+		mockMvc.perform(get("/expenses/report/print")
+				.param("reportStartDate", "2020-01-25")
+				.param("reportEndDate", "2019-12-31"))
+		
+		// then
+		.andExpect(status().isBadRequest());
 	}
 }
