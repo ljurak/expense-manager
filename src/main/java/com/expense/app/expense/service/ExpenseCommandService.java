46c1e5717b2c5cc44561613459dcfc23867fb4f1
ljurak <lucas.jurak@gmail.com>
2020-02-11 17:29:17 +0100

    Remove unused service classes

--- src/main/java/com/expense/app/expense/service/ExpenseCommandService.java
+++
@@ -1,9 +0,0 @@
-package com.expense.app.expense.service;
-
-import com.expense.app.expense.dto.command.ExpenseCreateCommand;
-import com.expense.app.expense.dto.command.ExpenseDeleteCommand;
-
-public interface ExpenseCommandService {
-	void createExpense(ExpenseCreateCommand command);
-	void deleteExpense(ExpenseDeleteCommand command);
-}
