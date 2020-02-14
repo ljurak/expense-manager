293edb1371ed4081b784d8fcda0f2bae12066629
ljurak <lucas.jurak@gmail.com>
2020-02-14 22:54:27 +0100

    Add methods for sending registration and password reset emails

--- src/main/java/com/expense/app/common/mail/service/EmailService.java
+++ src/main/java/com/expense/app/common/mail/service/EmailService.java
@@ -1,7 +1,12 @@
 package com.expense.app.common.mail.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 public interface EmailService {
 	void sendEmail(SimpleMailMessage mail);
+	void sendEmail(MimeMessage mail);
+	void sendRegistrationEmail(String to, String token, String verifyUrl);
+	void sendResetPasswordEmail(String to, String token, String resetUrl);
 }
