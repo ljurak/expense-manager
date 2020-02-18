9c9f1a975aefbfde5d7219e6e445317813cd20ee
ljurak <lucas.jurak@gmail.com>
2020-02-18 23:48:18 +0100

    Add command handler for password update

---
+++ src/main/java/com/expense/app/user/service/command/UserUpdatePasswordCommandHandler.java
@@ -0,0 +1,36 @@
+package com.expense.app.user.service.command;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.expense.app.common.cqrs.command.CommandHandler;
+import com.expense.app.user.dto.command.UserUpdatePasswordCommand;
+import com.expense.app.user.entity.UserEntity;
+import com.expense.app.user.exception.UpdatePasswordException;
+import com.expense.app.user.exception.UserNotFoundException;
+import com.expense.app.user.repo.UserRepo;
+
+@Service
+public class UserUpdatePasswordCommandHandler implements CommandHandler<UserUpdatePasswordCommand> {
+	
+	private UserRepo userRepo;
+	
+	private PasswordEncoder passwordEncoder;
+	
+	public UserUpdatePasswordCommandHandler(UserRepo userRepo, PasswordEncoder passwordEncoder) {
+		this.userRepo = userRepo;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Override
+	@Transactional
+	public void handle(UserUpdatePasswordCommand command) {
+		UserEntity user = userRepo.findByUsername(command.getUsername())
+				.orElseThrow(() -> new UserNotFoundException("User [" + command.getUsername() + "] has not been found"));
+		if (!passwordEncoder.matches(command.getCurrentPassword(), user.getPassword().replace("{bcrypt}", ""))) {
+			throw new UpdatePasswordException("Invalid password");
+		}
+		user.setPassword("{bcrypt}" + passwordEncoder.encode(command.getNewPassword()));
+	}	
+}
